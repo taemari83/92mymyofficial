9052710e5dc5bb501eebbb7cd53fb65a54403b59
taemari83 <taemari83@gmail.com>
2026-02-09 14:49:01 +0800

    Update main.ts

--- 4-of-92mymy-shop-system (7)/src/main.ts
+++ 4-of-92mymy-shop-system (7)/src/main.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app.component';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth'; // 1. 補上這行 import
 import { provideRouter } from '@angular/router';
 import { environment } from './environments/environment';
 import { routes } from './app.routes';
@@ -12,6 +13,7 @@ bootstrapApplication(AppComponent, {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()), // 2. 補上這行提供者，這樣 Ah (Auth) 就找得到了！
     provideFirestore(() => getFirestore())
   ]
 }).catch(err => console.error(err));
