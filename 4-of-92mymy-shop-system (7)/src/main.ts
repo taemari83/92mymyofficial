b5c4d73109e20312ab2d634eac0e8bba91d74bc1
taemari83 <taemari83@gmail.com>
2026-02-09 15:02:04 +0800

    Update main.ts

--- 4-of-92mymy-shop-system (7)/src/main.ts
+++ 4-of-92mymy-shop-system (7)/src/main.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app.component';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideAuth, getAuth } from '@angular/fire/auth'; // 1. 補上這行 import
+import { provideAuth, getAuth } from '@angular/fire/auth'; // 1. 新增這行：匯入驗證模組
 import { provideRouter } from '@angular/router';
 import { environment } from './environments/environment';
 import { routes } from './app.routes';
@@ -12,8 +12,11 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    // 初始化 Firebase
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()), // 2. 補上這行提供者，這樣 Ah (Auth) 就找得到了！
+    // 2. 新增這行：提供驗證服務 (這就是原本缺少的 Ah !)
+    provideAuth(() => getAuth()), 
+    // 提供資料庫服務
     provideFirestore(() => getFirestore())
   ]
 }).catch(err => console.error(err));
